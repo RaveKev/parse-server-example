44d7bf03e3daf55da26c61c65a283b900f1ed06a
Kevin Busch <busch.kevin@gmx.de>
2018-09-14 22:27:53 +0200

    r

--- cloud/functions/question/receive.js
+++ cloud/functions/question/receive.js
@@ -3,12 +3,11 @@ Parse.Cloud.define("receiveQuestionOverviewByUser", function(req, res){
     var currentUser = req.user;
     var limit = (req.limit === undefined) ? 5 : req.limit;
     
-    var Question = Parse.Object.extend("Question");
-    var qQuestion = new Parse.Query(Question);
-    qQuestion.limit(limit);
+    var qQuestion = new Parse.Query("Question");
+    /*qQuestion.limit(limit);
     qQuestion.descending("createdAt");
-    //qQuestion.equalTo("creator", currentUser);
-
+    qQuestion.equalTo("creator", currentUser);
+    */
 qQuestion.find().then(function(results) {
     response.success(results);
   }, function(error) {
