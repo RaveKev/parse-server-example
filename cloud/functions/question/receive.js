0bd4b9715f2070c9df84ececfa3d19c918d19ed8
Kevin Busch <busch.kevin@gmx.de>
2018-09-14 21:53:02 +0200

    added received

--- cloud/functions/question/receive.js
+++ cloud/functions/question/receive.js
@@ -1,6 +1,8 @@
 Parse.Cloud.define("receiveQuestionOverviewByUser", function(req, res){
     
     var currentUser = req.user;
+    var limit = (req.limit === undefined) ? 5 : req.limit;
+    
     var Question = Parse.Object.extend("Question");
     var qQuestion = new Parse.Query(Question);
     qQuestion.limit(limit);
