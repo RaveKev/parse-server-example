335b77bff33cfac67f5652764e295ad6b5dca4d5
Kevin Busch <busch.kevin@gmx.de>
2018-09-15 08:30:42 +0200

    try

--- cloud/triggers/answer/create.js
+++ cloud/triggers/answer/create.js
@@ -2,7 +2,21 @@ Parse.Cloud.afterSave("Answer", function(request) {
     console.log(request.object);
     console.log("Try to edit the answers an voters");
 
-  const query = new Parse.Query("Question");
+
+const query = new Parse.Query("Question");
+  query.get(request.object.get("question"))
+    .then(function(post) {
+      post.increment("voters", +1);
+      return post.save();
+    })
+    .catch(function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    });
+
+
+
+
+  /*const query = new Parse.Query("Question");
   query.get(request.object.get("question").id)
     .then(function(question) {
         console.log("found a question!");
@@ -21,5 +35,5 @@ Parse.Cloud.afterSave("Answer", function(request) {
     })
     .catch(function(error) {
       console.error("Got an error " + error.code + " : " + error.message);
-    });
+    });*/
 });
