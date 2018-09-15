570f2abe84f40567bd7b7801a062336076548ae2
Kevin Busch <busch.kevin@gmx.de>
2018-09-15 08:44:55 +0200

    omg

--- cloud/triggers/answer/create.js
+++ cloud/triggers/answer/create.js
@@ -3,24 +3,11 @@ Parse.Cloud.afterSave("Answer", function(request) {
     console.log("Try to edit the answers an voters");
 
 
-const query = new Parse.Query("Question");
-  query.get(request.object.get("question").id)
-    .then(function(post) {
-      post.increment("voters", 1);
-      return post.save();
-    })
-    .catch(function(error) {
-      console.error("Got an error " + error.code + " : " + error.message);
-    });
 
 
-
-
-  /*const query = new Parse.Query("Question");
+  const query = new Parse.Query("Question");
   query.get(request.object.get("question").id)
     .then(function(question) {
-        console.log("found a question!");
-
         question.increment("voters", 1);
         if(request.object.get("answer") == "Y"){
             question.increment("votedYes", 1);
@@ -28,12 +15,9 @@ const query = new Parse.Query("Question");
         if(request.object.get("answer") == "N"){
             question.increment("votedNo", 1);
         }
-
-      console.log("after incrementing");
-      console.log(question);
       return question.save(null, {useMasterKey:true});
     })
     .catch(function(error) {
       console.error("Got an error " + error.code + " : " + error.message);
-    });*/
+    });
 });
--- cloud/triggers/question/create.js
+++ cloud/triggers/question/create.js
@@ -1,5 +1,7 @@
 Parse.Cloud.beforeSave("Question", function(request, response) {
-    request.object.set("votedNo", 0);
-    request.object.set("votedYes", 0);
-    request.object.set("voters", 0);
+    if (request.object.existed() === false){
+        request.object.set("votedNo", 0);
+        request.object.set("votedYes", 0);
+        request.object.set("voters", 0);
+    } 
 });
