612dd54ddd5bde22ab75daac764633a76eb8395c
Kevin Busch <busch.kevin@gmx.de>
2018-09-15 08:08:24 +0200

    d

--- cloud/triggers/question/create.js
+++ cloud/triggers/question/create.js
@@ -3,6 +3,4 @@ Parse.Cloud.beforeSave("Question", function(request, response) {
     request.object.set("votedNo", 0);
     request.object.set("votedYes", 0);
     request.object.set("voters", 0);
-    
-    response.success();
 });
