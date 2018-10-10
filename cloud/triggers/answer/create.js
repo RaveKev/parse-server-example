0c65d295234bb83b76772967802c420aa2850e78
Kevin Busch <busch.kevin@gmx.de>
2018-10-10 06:22:48 +0200

    Console Logs

--- cloud/triggers/answer/create.js
+++ cloud/triggers/answer/create.js
@@ -2,9 +2,6 @@ Parse.Cloud.afterSave("Answer", function(request) {
     console.log(request.object);
     console.log("Try to edit the answers an voters");
 
-
-
-
   const query = new Parse.Query("Question");
   query.get(request.object.get("question").id)
     .then(function(question) {
@@ -20,4 +17,8 @@ Parse.Cloud.afterSave("Answer", function(request) {
     .catch(function(error) {
       console.error("Got an error " + error.code + " : " + error.message);
     });
+});
+
+Parse.Cloud.beforeSave("Answer", function(request, response) {
+
 });
--- cloud/triggers/question/create.js
+++ cloud/triggers/question/create.js
@@ -1,4 +1,6 @@
 Parse.Cloud.beforeSave("Question", function(request, response) {
+    console.log("beforeSave");
+    console.log(request);
     if (request.object.existed() === false){
         request.object.set("votedNo", 0);
         request.object.set("votedYes", 0);
