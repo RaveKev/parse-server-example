8a4ae1e192e70d312ad085395a68f755c11f6cd9
Kevin Busch <busch.kevin@gmx.de>
2018-10-11 00:00:45 +0200

    added value

--- cloud/triggers/question/create.js
+++ cloud/triggers/question/create.js
@@ -5,5 +5,7 @@ Parse.Cloud.beforeSave("Question", function(request, response) {
         request.object.set("voted0", 0);
         request.object.set("voted1", 0);
         request.object.set("voters", 0);
-    } 
+        request.object.set("value0", "Nein");
+        request.object.set("value1", "Ja");
+    }
 });
