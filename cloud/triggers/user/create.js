905fefab0b8ca13a821db04c9c1945e193a2c2b5
Kevin Busch <busch.kevin@gmx.de>
2018-10-10 07:56:05 +0200

    UserCreationTrigger

--- cloud/triggers/user/create.js
+++ cloud/triggers/user/create.js
@@ -1,12 +1,13 @@
 Parse.Cloud.afterSave(Parse.User, function(request) {
     console.log("Parse.Cloud.afterSave: ");
     request.log.info("Parse.Cloud.afterSave: "); // For back4app user
-
+    console.log(request);
+    
     console.log(request.object);
     if (request.object.existed() === false) {
         var Profile = Parse.Object.extend("Profile");
         var qProfile = new Profile();
-        qProfile.set("user", request.object.id);
+        qProfile.set("user", request.object);
         qProfile.save();
     }
 });
