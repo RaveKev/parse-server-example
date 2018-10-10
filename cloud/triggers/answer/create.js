d3b1b3d9f6907ed0c2c610a83e82fa932d08b7ac
Kevin Busch <busch.kevin@gmx.de>
2018-10-10 06:53:32 +0200

    logs

--- cloud/triggers/answer/create.js
+++ cloud/triggers/answer/create.js
@@ -1,28 +1,27 @@
 Parse.Cloud.afterSave("Answer", function(request) {
-    console.log(request.object);
-    console.log(request);
-    console.log("Answerer Attributes:");
-    console.log(request.object.get("answerer").attributes);
-    console.log("Try to edit the answers an voters");
+    console.log('*** Cloud afteresave\'s request = ', request);
+    var user = request.user;
+    console.log(user);
 
-
-  const query = new Parse.Query("Question");
-  query.get(request.object.get("question").id)
-    .then(function(question) {
-        question.increment("voters", 1);
-        if(request.object.get("answer") == "Y"){
-            question.increment("votedYes", 1);
-        }
-        if(request.object.get("answer") == "N"){
-            question.increment("votedNo", 1);
-        }
-      return question.save(null, {useMasterKey:true});
-    })
-    .catch(function(error) {
-      console.error("Got an error " + error.code + " : " + error.message);
-    });
+    const query = new Parse.Query("Question");
+    query.get(request.object.get("question").id)
+        .then(function(question) {
+            question.increment("voters", 1);
+            if(request.object.get("answer") == "Y"){
+                question.increment("votedYes", 1);
+            }
+            if(request.object.get("answer") == "N"){
+                question.increment("votedNo", 1);
+            }
+          return question.save(null, {useMasterKey:true});
+        })
+        .catch(function(error) {
+          console.error("Got an error " + error.code + " : " + error.message);
+        });
 });
 
 Parse.Cloud.beforeSave("Answer", function(request, response) {
-
+    console.log('*** Cloud beforesave\'s request = ', request);
+    var user = request.user;
+    console.log(user);
 });
